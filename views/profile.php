d34c7253806b0e78702cd70a711967aad2ea2cd8
mohammed873 <57411879+mohammed873@users.noreply.github.com>
2020-09-04 21:22:33 +0000

    Update profile.php
    new update

--- views/profile.php
+++ views/profile.php
@@ -364,6 +364,10 @@ Copyright &copy;<script>document.write(new Date().getFullYear());</script> All r
   src="https://code.jquery.com/jquery-3.5.1.js"
   integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc="
   crossorigin="anonymous"></script>
+   <script
+  <!-- custom js -->
+<script src="../assests/js/profile.js"></script>
+  
   
 </body> 
 </html>
